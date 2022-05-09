63a073312b6e68885b2962a039d0dbc586705dc5
Rostyslav <malanyukrostik@gmail.com>
2022-05-09 11:41:00 +0300

    edit admin panel

--- src/users/admin.py
+++ src/users/admin.py
@@ -3,4 +3,5 @@ from users. models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    exclude = ("user_permissions", "groups")
+    readonly_fields = ("phone_number", "username", "fullname", "car", "sex", "last_login")
+    exclude = ("user_permissions", "groups", "password") 
